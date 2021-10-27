7cf6b8fc0b09a23fc85f31509f209d4a1d4e94c2
lashari39 <obaid.lashari@square63.org>
2021-10-27 15:36:47 +0500

    Added username in the sign_up form

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,5 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :password ,format: {with: /(?=.{8,})(?=.*[A-Z])(?=.*[[:^alnum:]])/ , message: 'does not contain one Uppercase and one Special characher'}
+  validates :username, length: { maximum: 30 , message: 'Maximun limit is 30 characters' } , presence: true
 end
