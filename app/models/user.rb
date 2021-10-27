05563c8d8cd85eadf101dc79d56442b3d8b5001e
lashari39 <obaid.lashari@square63.org>
2021-10-27 13:58:36 +0500

    Validated the email/phone and password

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :password ,format: {with: /(?=.{8,})(?=.*[A-Z])(?=.*[[:^alnum:]])/ , message: 'does not contain one Uppercase and one Special characher'}
 end
