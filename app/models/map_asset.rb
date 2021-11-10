74d3c6907982ddf86d0254a4715220dc13167ccc
lashari39 <obaid.lashari@square63.org>
2021-11-10 09:50:17 +0500

    Created asset model

---
+++ app/models/map_asset.rb
@@ -0,0 +1,2 @@
+class MapAsset < ApplicationRecord
+end
---
+++ db/migrate/20211110044758_create_map_assets.rb
@@ -0,0 +1,14 @@
+class CreateMapAssets < ActiveRecord::Migration[6.1]
+  def change
+    create_table :map_assets do |t|
+      t.string :place
+      t.string :address
+      t.string :category
+      t.string :rate
+      t.string :review
+      t.string :description
+
+      t.timestamps
+    end
+  end
+end
