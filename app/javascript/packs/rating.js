99ff85ad46a8a3d7509f1f886fa6ee1d0ad37c79
lashari39 <obaid.lashari@square63.org>
2021-11-18 18:52:25 +0500

    Resolved jquery and form issues of asset

--- app/javascript/packs/rating.js
+++ app/javascript/packs/rating.js
@@ -1,83 +1,99 @@
 window.bind_rating = function() {
-	$('#bad').on("click", function() {
+	$('.bad').on("click", function() {
 		tog = $(this);
-		if (tog.attr("class") == "btn btn-outline-danger")
+		if (tog.hasClass("btn-outline-danger"))
 		{
-			tog.removeClass('btn btn-outline-danger').addClass('btn btn-danger');
-			$('#review').val("bad");
-			$('#good').removeClass().addClass('btn btn-outline-success')
-			$('#fix').removeClass().addClass('btn btn-outline-info')
+			tog.removeClass('btn-outline-danger').addClass('btn-danger');
+			$('.review').val("bad");
+			$('.good').removeClass().addClass('good btn btn-outline-success')
+			$('.fix').removeClass().addClass('fix btn btn-outline-info')
 		}
 		else
 		{
-			tog.removeClass('btn btn-danger').addClass('btn btn-outline-danger');
-			$('#review').val("");
+			tog.removeClass('btn-danger').addClass('btn-outline-danger');
+			$('.review').val("");
 		}
 	});
 
-	$('#fix').on("click", function() {
+	$('.fix').on("click", function() {
 		tog = $(this);
-		if (tog.attr("class") == "btn btn-outline-info")
+		if (tog.hasClass("btn-outline-info"))
 		{
-			tog.removeClass('btn btn-outline-info').addClass('btn btn-info');
-			$('#review').val("fix");
-			$('#bad').removeClass().addClass('btn btn-outline-danger')
-			$('#good').removeClass().addClass('btn btn-outline-success')
+			tog.removeClass('btn-outline-info').addClass('btn-info');
+			$('.review').val("fix");
+			$('.bad').removeClass().addClass('bad btn btn-outline-danger')
+			$('.good').removeClass().addClass('good btn btn-outline-success')
 		}
 		else
 		{
-			tog.removeClass('btn btn-info').addClass('btn btn-outline-info');
-			$('#review').val("");
+			tog.removeClass('btn-info').addClass('btn-outline-info');
+			$('.review').val("");
 		}
 	});
 
-	$('#edit-good').on("click", function() {
+		$('.good').on("click", function() {
 		tog = $(this);
-		if (tog.attr("class") == "btn btn-outline-success")
+		if (tog.hasClass("btn-outline-success"))
 		{
-			tog.removeClass('btn btn-outline-success').addClass('btn btn-success');
-			$('#edit-review').val("good");
-			$('#edit-bad').removeClass().addClass('btn btn-outline-danger')
-			$('#edit-fix').removeClass().addClass('btn btn-outline-info')
+			tog.removeClass('btn-outline-success').addClass('btn-success');
+			$('.review').val("good");
+			$('.bad').removeClass().addClass('bad btn btn-outline-danger')
+			$('.fix').removeClass().addClass('fix btn btn-outline-info')
 		}
 		else
 		{
-			tog.removeClass('btn btn-success').addClass('btn btn-outline-success');
-			$('#edit-review').val("");
+			tog.removeClass('btn-success').addClass('btn-outline-success');
+			$('.review').val("");
 		}
 	});
 
-	$('#edit-bad').on("click", function() {
-		tog = $(this);
-		if (tog.attr("class") == "btn btn-outline-danger")
-		{
-			tog.removeClass('btn btn-outline-danger').addClass('btn btn-danger');
-			$('#edit-review').val("bad");
-			$('#edit-good').removeClass().addClass('btn btn-outline-success')
-			$('#edit-fix').removeClass().addClass('btn btn-outline-info')
-		}
-		else
-		{
-			tog.removeClass('btn btn-danger').addClass('btn btn-outline-danger');
-			$('#edit-review').val("");
-		}
-	});
+	// $('#edit-good').on("click", function() {
+	// 	tog = $(this);
+	// 	if (tog.attr("class") == "btn btn-outline-success")
+	// 	{
+	// 		tog.removeClass('btn btn-outline-success').addClass('btn btn-success');
+	// 		$('#edit-review').val("good");
+	// 		$('#edit-bad').removeClass().addClass('btn btn-outline-danger')
+	// 		$('#edit-fix').removeClass().addClass('btn btn-outline-info')
+	// 	}
+	// 	else
+	// 	{
+	// 		tog.removeClass('btn btn-success').addClass('btn btn-outline-success');
+	// 		$('#edit-review').val("");
+	// 	}
+	// });
 
-	$('#edit-fix').on("click", function() {
-		tog = $(this);
-		if (tog.attr("class") == "btn btn-outline-info")
-		{
-			tog.removeClass('btn btn-outline-info').addClass('btn btn-info');
-			$('#edit-review').val("fix");
-			$('#edit-bad').removeClass().addClass('btn btn-outline-danger')
-			$('#edit-good').removeClass().addClass('btn btn-outline-success')
-		}
-		else
-		{
-			tog.removeClass('btn btn-info').addClass('btn btn-outline-info');
-			$('#review').val("");
-		}
-	});
+	// $('#edit-bad').on("click", function() {
+	// 	tog = $(this);
+	// 	if (tog.attr("class") == "btn btn-outline-danger")
+	// 	{
+	// 		tog.removeClass('btn btn-outline-danger').addClass('btn btn-danger');
+	// 		$('#edit-review').val("bad");
+	// 		$('#edit-good').removeClass().addClass('btn btn-outline-success')
+	// 		$('#edit-fix').removeClass().addClass('btn btn-outline-info')
+	// 	}
+	// 	else
+	// 	{
+	// 		tog.removeClass('btn btn-danger').addClass('btn btn-outline-danger');
+	// 		$('#edit-review').val("");
+	// 	}
+	// });
+
+	// $('#edit-fix').on("click", function() {
+	// 	tog = $(this);
+	// 	if (tog.attr("class") == "btn btn-outline-info")
+	// 	{
+	// 		tog.removeClass('btn btn-outline-info').addClass('btn btn-info');
+	// 		$('#edit-review').val("fix");
+	// 		$('#edit-bad').removeClass().addClass('btn btn-outline-danger')
+	// 		$('#edit-good').removeClass().addClass('btn btn-outline-success')
+	// 	}
+	// 	else
+	// 	{
+	// 		tog.removeClass('btn btn-info').addClass('btn btn-outline-info');
+	// 		$('#review').val("");
+	// 	}
+	// });
 }
 
 $(document).on('turbolinks:load', function(){
