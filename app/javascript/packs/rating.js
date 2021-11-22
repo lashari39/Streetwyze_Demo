2397bd6fcbade8d2068faeac2923902296bb97eb
lashari39 <obaid.lashari@square63.org>
2021-11-22 15:13:01 +0500

    Refactored the jquery code

--- app/javascript/packs/rating.js
+++ app/javascript/packs/rating.js
@@ -1,52 +1,63 @@
 window.bind_rating = function() {
-	$('.bad').on("click", function() {
-		tog = $(this);
-		if (tog.hasClass("btn-outline-danger"))
+	$('.review-button').on('click', function() {
+		reviewButton = $(this)
+		if(reviewButton.hasClass("bad"))
 		{
-			tog.removeClass('btn-outline-danger').addClass('btn-danger');
-			$('.review').val("bad");
-			$('.good').removeClass().addClass('good btn btn-outline-success')
-			$('.fix').removeClass().addClass('fix btn btn-outline-info')
+			badButton(reviewButton);
+		}
+		else if(reviewButton.hasClass("fix"))
+		{
+			fixButton(reviewButton);
 		}
 		else
 		{
-			tog.removeClass('btn-danger').addClass('btn-outline-danger');
-			$('.review').val("");
-		}
-	});
-
-	$('.fix').on("click", function() {
-		tog = $(this);
-		if (tog.hasClass("btn-outline-info"))
-		{
-			tog.removeClass('btn-outline-info').addClass('btn-info');
-			$('.review').val("fix");
-			$('.bad').removeClass().addClass('bad btn btn-outline-danger')
-			$('.good').removeClass().addClass('good btn btn-outline-success')
-		}
-		else
-		{
-			tog.removeClass('btn-info').addClass('btn-outline-info');
-			$('.review').val("");
-		}
-	});
-
-		$('.good').on("click", function() {
-		tog = $(this);
-		if (tog.hasClass("btn-outline-success"))
-		{
-			tog.removeClass('btn-outline-success').addClass('btn-success');
-			$('.review').val("good");
-			$('.bad').removeClass().addClass('bad btn btn-outline-danger')
-			$('.fix').removeClass().addClass('fix btn btn-outline-info')
-		}
-		else
-		{
-			tog.removeClass('btn-success').addClass('btn-outline-success');
-			$('.review').val("");
+			goodButton(reviewButton);
 		}
 	});
 }
+function badButton(badButton) {
+	if (badButton.hasClass("btn-outline-danger"))
+		{
+			badButton.removeClass('btn-outline-danger').addClass('btn-danger');
+			$('.review').val("bad");
+			$('.good').removeClass().addClass('review-button good btn btn-outline-success');
+			$('.fix').removeClass().addClass('review-button fix btn btn-outline-info');
+		}
+		else
+		{
+			badButton.removeClass('btn-danger').addClass('btn-outline-danger');
+			$('.review').val("");
+		}
+};
+
+function goodButton(goodButton) {
+	if (goodButton.hasClass("btn-outline-success"))
+		{
+			goodButton.removeClass('btn-outline-success').addClass('btn-success');
+			$('.review').val("good");
+			$('.bad').removeClass().addClass('review-button bad btn btn-outline-danger');
+			$('.fix').removeClass().addClass('review-button fix btn btn-outline-info');
+		}
+		else
+		{
+			goodButton.removeClass('btn-success').addClass('btn-outline-success');
+			$('.review').val("");
+		}
+};
+function fixButton(fixButton) {
+	if (fixButton.hasClass("btn-outline-info"))
+		{
+			fixButton.removeClass('btn-outline-info').addClass('btn-info');
+			$('.review').val("fix");
+			$('.bad').removeClass().addClass('review-button bad btn btn-outline-danger');
+			$('.good').removeClass().addClass('review-button good btn btn-outline-success');
+		}
+		else
+		{
+			fixButton.removeClass('btn-info').addClass('btn-outline-info');
+			$('.review').val("");
+		}
+};
 
 $(document).on('turbolinks:load', function(){
 	bind_rating();
